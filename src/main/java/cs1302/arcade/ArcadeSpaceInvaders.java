a0fd00be7186f6836d99840de932b5d7f87a794e
Gi Suen (G) <gi.suen@uga.edu>
2019-04-30 21:06:08 -0400

    repositioned the player and fixed a winning bug

--- src/main/java/cs1302/arcade/ArcadeSpaceInvaders.java
+++ src/main/java/cs1302/arcade/ArcadeSpaceInvaders.java
@@ -23,7 +23,7 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-//Some of this code was used as reference will not use in final submission
+//Somes of this code was used as reference will not use in final submission
 // Just used to test to see if code worked.
 /**
  *This is Arcade game called Space Invaders
