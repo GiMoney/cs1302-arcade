9cc0e6bd2465ed22faad4e33d0d54d85bf7f50e9
Kyle Mettille <kam72069@uga.edu>
2019-05-07 12:37:18 -0400

    finishing touches on Board2048

--- src/main/java/cs1302/arcade/Board2048.java
+++ src/main/java/cs1302/arcade/Board2048.java
@@ -29,10 +29,10 @@ public class Board2048 extends Group {
     private Tile[][] board; /** Array that holds all the Tiles in the game */
     private boolean emptyMove; /** Used to check if a move is "empty" */
     private int score; /** value all of tiles on the board */
-    private Text textScore;
-    private Rectangle scoreBox;
-    private Alert loseAlert;
-    private Alert winAlert;
+    private Text textScore; /** displays the score on the scene */
+    private Rectangle scoreBox; /** rectangle used as a background for score */
+    private Alert loseAlert; /** alert that notifies user that they lost */
+    private Alert winAlert; /** alert that notifies user they got a 2048 */
 
     /**
      * Default constructor that is used to create a new game of 2048. Each
@@ -54,28 +54,33 @@ public class Board2048 extends Group {
 	current.setFont(new Font(18));
 	textScore = new Text(450, 90, "" + score);
 	textScore.setFill(Color.WHITE);
-	textScore.setFont(new Font(50));
+	textScore.setFont(new Font(40));
 	textScore.setTextAlignment(TextAlignment.CENTER);
 	scoreBox = new Rectangle(440, 0, 150, 100);
 	scoreBox.setFill(Color.GRAY);
 	scoreBox.setArcWidth(20);
-	scoreBox.setArcHeight(20);
+	scoreBox.setArcHeight(20); //all of this creates a neat space for display the score
 	this.getChildren().addAll(scoreBox, current, textScore);
 	this.randTile();
-	this.randTile();
+	this.randTile(); //creates two random tiles on the board
 	this.setOnKeyPressed(this.createKeyEvent());
 	Platform.runLater(() -> loseAlert = new Alert(AlertType.INFORMATION, "You lose!"));
 	Platform.runLater(() -> winAlert = new Alert(AlertType.INFORMATION, "You win! You can continue" +
-						     " playing to see how high of score you can get!"));
+						     " playing to see how high of a score you can get!"));
     }//board constructor
 
+    /**
+     * Used to create a new game of 2048 if the player desires to start
+     * over or has lost. The score will be reset but the score of the last
+     * run will still be display until the user makes another move.
+     */
     public void newGame() {
 	score = 0;
 	for(int row = 0; row < board.length; row++) {
 	    for(int col = 0; col < board[row].length; col++) {
 		board[row][col].setNum(0);
-	    }
-	}
+	    }//for - goes through columns and resets Tiles
+	}//for - goes through rows
 	this.randTile();
 	this.randTile();
     }//newGame
@@ -88,7 +93,6 @@ public class Board2048 extends Group {
     private EventHandler<? super KeyEvent> createKeyEvent() {
 	return event -> {
 	    Runnable r = () -> {
-		System.out.println(event);
 		if(event.getCode() == KeyCode.LEFT) {
 		    this.left();
 		}//if - left key to move left
@@ -101,11 +105,11 @@ public class Board2048 extends Group {
 		if(event.getCode() == KeyCode.DOWN) {
 		    this.down();
 		}//if - down key to move down
-	    };
+	    }; //implementation of Runnable for thread
 	    Thread t = new Thread(r);
 	    t.setDaemon(true);
 	    t.start();
-	};
+	};//returns an event used for keyboard controls
     }//createKeyEvent
 
     /**
@@ -116,9 +120,9 @@ public class Board2048 extends Group {
 	    for(int col = 1; col < board[row].length; col++) {
 		for(int i = col; i > 0; i--) {
 		    this.move(row, i, row, i - 1);
-		}
-	    }
-	}//for
+		}//for - moves a singular tile up
+	    }//for - goes through column low to high
+	}//for - goes through rows
 	this.spawn();
     }//left
 
@@ -130,8 +134,8 @@ public class Board2048 extends Group {
 	    for(int col = board[row].length - 2; col >= 0; col--) {
 		for(int i = col; i < board.length - 1; i++) {
 		    this.move(row, i, row, i + 1);
-		}
-	    }
+		}//for - moves a singular tile down
+	    }//for - goes through columns high to low
 	}//for
 	this.spawn();
     }//right
@@ -144,9 +148,9 @@ public class Board2048 extends Group {
 	    for(int row = board.length - 2; row >= 0; row--) {
 		for(int i = row; i < board.length - 1; i++) {
 		    this.move(i, col, i + 1, col);
-		}
-	    }
-	}//for
+		}//for - moves a singular tile right
+	    }//for - goes through rows high to low
+	}//for - goes through columns
 	this.spawn();
     }//up
 
@@ -158,9 +162,9 @@ public class Board2048 extends Group {
 	    for(int row = 1; row < board.length; row++) {
 		for(int i = row; i > 0; i--) {
 		    this.move(i, col, i - 1, col);
-		}
-	    }
-	}//for
+		}//for - moves a singular tile left
+	    }//for - goes through rows low to high
+	}//for - goes through columns
 	this.spawn();
     }//down
 
@@ -179,35 +183,33 @@ public class Board2048 extends Group {
     public void move(int x1, int y1, int x2, int y2) {
 	if(board[x2][y2].isEmpty() && !board[x1][y1].isEmpty()) {
 	    board[x2][y2].setNum(board[x1][y1].getNum());
-	    board[x1][y1].setNum(0);
-	    System.out.println("moved");
-	    emptyMove = false;
+	    board[x1][y1].setNum(0); //if tile destination is empty, then
+	    emptyMove = false;       //the tile will move to empty slot  
 	}else if(!board[x1][y1].isEmpty() && !board[x2][y2].isEmpty() &&
 		 board[x1][y1].getNum() == board[x2][y2].getNum() &&
 		 board[x1][y1].getNum() < 2048) {
 	    board[x2][y2].setNum(board[x2][y2].getNum() +
 				 board[x1][y1].getNum());
-	    board[x1][y1].setNum(0);
+	    board[x1][y1].setNum(0); //joins two numbers of same value
 	    if(board[x2][y2].getNum() == 2048) {
 		Platform.runLater(() -> winAlert.showAndWait());
-	    }
-	    score += board[x2][y2].getNum();
-	    textScore.setText("" + score);
-	    System.out.println("joined");
+	    }//if - 2048 tile is created, a win message will be displayed
+	    score += board[x2][y2].getNum(); //updates score
+	    Platform.runLater(() -> textScore.setText("" + score));
 	    emptyMove = false;
-	}
+	}//if-else - checks to see if a move is "swap" or a "join"
     }//move
 
     /**
-     * Spawns a random {@code Tile} IF IT IS NOT AN EMPTY MOVE!!!
+     * Spawns a random {@code Tile} IF IT IS NOT AN EMPTY MOVE!!! Will
+     * also check to see if a player has lost if the board fills up with
+     * a spawned tile.
      */
     public void spawn() {
 	if(!checkEmptyMove()) {
 	    this.randTile();
-	    this.checkLose();
-	}else {
-	    System.out.println("empty move");
-	}
+	    this.checkLose(); //checks if the player has lost
+	}//checks to see if move is NOT empty
     }//spawn
 
     /** 
@@ -217,13 +219,13 @@ public class Board2048 extends Group {
      */
     public void randTile() {
 	int rand = 2 * ((int)(Math.random()) * 2 + 1);
-	int x;
-	int y;
+	int x; //x coord
+	int y; //y coord
 	do {
 	    x = (int)(4 * Math.random());
 	    y = (int)(4 * Math.random());
-	}while(!board[x][y].isEmpty());
-	board[x][y].setNum(rand);
+	}while(!board[x][y].isEmpty()); //finds empty spot
+	board[x][y].setNum(rand); //sets empty tile to either 2 or 4
     }//randTile
 
     /**
@@ -244,37 +246,50 @@ public class Board2048 extends Group {
 	return true;
     }//checkEmptyMove
 
+    /**
+     * Checks to see if the player has lost the game. This means that the
+     * board is full and there are no possible moves left. Makes use
+     * of a another method {@code checkPossible} to see if board is possible
+     * if full.
+     */
     private void checkLose() {
 	boolean fullBoard = true;
 	for(int r = 0; r < board.length; r++) {
 	    for(int c = 0; c < board[r].length; c++) {
 		if(board[r][c].getNum() == 0) {
 		    fullBoard = false;
-		}
-	    }
-	}
+		}//if - tile is empty then board isn't full
+	    }//for - checks columns
+	}//for - checks rows
 	if(fullBoard){
 	    if(!this.checkPossible()) {
 		Platform.runLater(() -> loseAlert.showAndWait());
-	    }
-	}
-    }
+	    }//if - impossible, then prompt lose alert
+	}//if - board is full of tiles
+    }//checkLose
 
+    /**
+     * Checks to see if a board has any possible moves ASSUMING the board
+     * is full (since this is a helper method for {@code checkLose}. This is
+     * done by checking each row and column and their "neighbors" inside them.
+     * "Neighbors" means the tiles directly next to the tile being checked.
+     * @return true if possible, false if impossible
+     */
     private boolean checkPossible() {
 	for(int r = 0; r < board.length; r++) {
 	    for(int c = 0; c < board[r].length - 1; c++) {
 		if(board[r][c].getNum() == board[r][c+1].getNum()) {
 		    return true;
-		}
-	    }
-	}
+		}//if - all column neighbors are different from each other
+	    }//for - checks columns
+	}//for - checks rows
 	for(int c = 0; c < board[0].length; c++) {
 	    for(int r = 0; r < board.length - 1; r++) {
 		if(board[r][c].getNum() == board[r+1][c].getNum()) {
 		    return true;
-		}
-	    }
-	}
+		}//if - all row neighbors are different from each other
+	    }//for - checks rows
+	}//for - checks columns
 	return false;
     }//checkPossible
 
@@ -297,9 +312,9 @@ public class Board2048 extends Group {
 	 */
 	public Tile(int x, int y) {
 	    super((x * 100) + (x * 10), (y * 100) + (y * 10), 100, 100);
-	    number = 0;
+	    number = 0; //sets tile to empty and coord based on array location
 	    this.setFill(Color.GRAY);
-	    this.setArcHeight(20);
+	    this.setArcHeight(20); //creates curved edges
 	    this.setArcWidth(20);
 	}//empty Tile constructor
 
@@ -314,8 +329,8 @@ public class Board2048 extends Group {
 		this.setFill(Color.GRAY);
 	    }else {
 		Image i = new Image("2048/" + num + ".jpg");
-		this.setFill(new ImagePattern(i));
-	    }
+		this.setFill(new ImagePattern(i)); //uses value to find image
+	    }//if-else - 0 for empty tile and other numbers for image files
 	}//setNum
 
 	/**
@@ -339,4 +354,4 @@ public class Board2048 extends Group {
 	    return false;
 	}//isEmpty
     }//Tile
-}
+}//Board2048
